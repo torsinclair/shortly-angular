c6d31c491536829dd925d7b5d7539ea9d1ea4924
Jonathan Mah <jonathan.mah.7@gmail.com>
2016-04-01 14:19:23 -0700

    Initial Commit.

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -1,7 +1,15 @@
 angular.module('shortly.services', [])
 
 .factory('Links', function ($http) {
-  // Your code here
+  var get = function() {
+    return $http({
+      method: 'GET',
+      url: '/api/links',
+      data: ''
+    }).then(function(resp) {
+      return resp.data;
+    });
+  };
 })
 .factory('Auth', function ($http, $location, $window) {
   // Don't touch this Auth service!!!
